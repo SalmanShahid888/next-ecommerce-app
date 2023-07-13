961c083280414320ed28fe215f75cf887a7941de
salman8896 <salmanshahid8896@gmail.com>
2023-07-13 19:39:24 +0500

    Added API Documentation component in Settings

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -12,11 +12,17 @@ const MainNav = ({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Dashboard",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
     },
   ];
+
   return (
     <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
       {routes.map((route) => (
